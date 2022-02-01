65761cc3bd2e18dfe524362a5377cdbd3f61875a
softdream815 <uyg0815@gmail.com>
2022-02-01 22:17:06 +0300

    construct layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Switch, Route } from 'react-router-dom';
+
+import Homepage from './pages/Homepage/Homepage';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Switch>
+      <Route path='/' component={Homepage} />
+    </Switch>
+  )
 }
 
 export default App;
---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import Layout from "../../layout/Layout"
+
+const Homepage = () => {
+  return (
+    <Layout>
+
+    </Layout>
+  )
+}
+
+export default Homepage
