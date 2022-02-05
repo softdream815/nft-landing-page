d3f1dc0a3ed7e8ae563c557e1c10192cac7c7c9f
softdream815 <uyg0815@gmail.com>
2022-02-05 05:19:52 +0300

    complete mobile header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -2,23 +2,41 @@ import { ReactComponent as Logo } from '../assets/icon/logo.svg';
 import { ReactComponent as Twitter } from '../assets/icon/twitter.svg';
 import { ReactComponent as Discord } from '../assets/icon/discord.svg';
 import { ReactComponent as Menu } from '../assets/icon/menu.svg';
+import Text from '../components/text/Text'
+import { useState, useEffect } from 'react'
 
 import { Link } from 'react-router-dom'
 import './style.scss'
 import $ from "jquery"
 
 const Header = () => {
+  const [isShow, setShow] = useState(false)
+
   const onLink = (type) => {
     $('html, body').animate(
       {
-        scrollTop: $(`#${type}`).offset().top-100,
+        scrollTop: $(`#${type}`).offset().top-200,
       },
       800
     )
+    setShow(false)
+  }
+
+  const showMenu = () => {
+    setShow(!isShow)
   }
 
   return (
     <div className='header'>
+      <div className={ isShow ? 'menu-bar expand' : 'menu-bar' }>
+        <div className='menu-item'><Link to='/' onClick={() => onLink("home")}><Text weight={300} size={16}>Home</Text></Link></div>
+        <div className='menu-item'><Link to='/' onClick={() => onLink("about")}><Text weight={300} size={16}>About</Text></Link></div>
+        <div className='menu-item'><Link to='/' onClick={() => onLink("howto")}><Text weight={300} size={16}>How-To</Text></Link></div>
+        <div className='menu-item'><Link to='/' onClick={() => onLink("roadmap")}><Text weight={300} size={16}>Roadmap</Text></Link></div>
+        <div className='menu-item'><Link to='/' onClick={() => onLink("faq")}><Text weight={300} size={16}>FAQs</Text></Link></div>
+        <div className='menu-item'><Link to='/'><Text weight={300} size={16}>Mint</Text></Link></div>
+      </div>
+
       <div className='logo'>
         <Link to='/'>
           <Logo></Logo>
@@ -37,7 +55,7 @@ const Header = () => {
       <div className='social-icons'>
         <Link to='/'><Twitter/></Link>
         <Link to='/'><Discord/></Link>
-        <Link to='/' className='menu'><Menu width={25} height={20}/></Link>
+        <Link to='/' onClick={showMenu} className='menu'><Menu width={25} height={20}/></Link>
       </div>
     </div>
   )
